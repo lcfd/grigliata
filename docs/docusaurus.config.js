ace120bb5ab493893101da153200e6197bee4b27
lcfd <9001053+fedriz@users.noreply.github.com>
2023-03-04 15:49:51 +0100

    Update documentation and fix typos

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,79 +1,106 @@
+// @ts-check
+
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-// With JSDoc @type annotations, IDEs can provide config autocompletion
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-(
-  module.exports = {
-    title: "Grigliata",
-    tagline: "A styled-components grid system.",
-    url: "https://lcfd.github.io",
-    baseUrl: "/grigliata/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.png",
-    organizationName: "lcfd", // Usually your GitHub org/user name.
-    projectName: "grigliata", // Usually your repo name.
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Grigliata",
+  tagline: "A styled-components grid system.",
+  url: "https://lcfd.github.io",
+  baseUrl: "/grigliata/",
 
-    presets: [
-      [
-        "@docusaurus/preset-classic",
-        /** @type {import('@docusaurus/preset-classic').Options} */
-        ({
-          docs: {
-            sidebarPath: require.resolve("./sidebars.js"),
-          },
-          theme: {
-            customCss: require.resolve("./src/css/custom.css"),
-          },
-        }),
-      ],
-    ],
+  favicon: "img/favicon.ico",
 
-    themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  // GitHub
+  organizationName: "lcfd", // Usually your GitHub org/user name.
+  projectName: "grigliata", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"]
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        navbar: {
-          title: "Grigliata",
-          logo: {
-            alt: "Logo",
-            src: "img/favicon.png",
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/lcfd/grigliata/docs"
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: "https://github.com/lcfd/grigliata/docs"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      title: "Grigliata",
+      logo: {
+        alt: "Grigliata Logo",
+        src: "img/favicon.png"
+      },
+      image: "img/social-card.jpg",
+      navbar: {
+        title: "Grigliata",
+        logo: {
+          alt: "Grigliata Logo",
+          src: "img/favicon.png"
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs"
           },
-          items: [
-            {
-              type: "doc",
-              docId: "intro",
-              position: "left",
-              label: "Docs",
-            },
-            { to: "about", label: "About", position: "left" },
-            { to: "roadmap", label: "Roadmap", position: "left" },
-            {
-              href: "https://github.com/lcfd/grigliata",
-              label: "GitHub",
-              position: "right",
-            },
-          ],
-        },
-        footer: {
-          style: "dark",
-          links: [
-            {
-              title: "Docs",
-              items: [
-                {
-                  label: "Tutorial",
-                  to: "/docs/intro",
-                },
-              ],
-            },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} Grigliata`,
-        },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
-        },
-      }),
-  }
-);
+          { to: "about", label: "About", position: "left" },
+          { to: "blog", label: "Blog", position: "left" },
+          { to: "roadmap", label: "Roadmap", position: "left" },
+          {
+            href: "https://github.com/lcfd/grigliata",
+            label: "GitHub",
+            position: "right"
+          },
+          {
+            href: "https://www.npmjs.com/package/grigliata",
+            label: "Npm",
+            position: "right"
+          }
+        ]
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro"
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Grigliata`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+};
+
+module.exports = config;
